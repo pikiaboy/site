e261917853cb5ef84f6ca7dcb630aee5116e5ab3
Thomas Pham <thomas.pham96@yahoo.com>
2020-12-08 21:21:38 -0800

    Adding prettier

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,12 +1,12 @@
 const commonConfig = require('./common.config');
-const TerserPlugin = require("terser-webpack-plugin");
-const { merge } = require("webpack-merge");
+const TerserPlugin = require('terser-webpack-plugin');
+const { merge } = require('webpack-merge');
 
 module.exports = merge(commonConfig, {
     mode: 'production',
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
-        splitChunks: { chunks: "all" }
-    },
+        splitChunks: { chunks: 'all' }
+    }
 });
