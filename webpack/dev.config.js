4490587f3dffd37b9af5981429a9756fd5a3d6bf
Thomas Pham <thomas.pham96@yahoo.com>
2020-12-08 17:30:58 -0800

    Splitting webpack configs to common, dev, and prod (#2)

---
+++ webpack/dev.config.js
@@ -0,0 +1,7 @@
+const commonConfig = require('./common.config');
+const { merge } = require("webpack-merge");
+
+module.exports = merge(commonConfig, {
+    mode: 'development',
+    devtool: "source-map",
+});
---
+++ webpack/prod.config.js
@@ -0,0 +1,12 @@
+const commonConfig = require('./common.config');
+const TerserPlugin = require("terser-webpack-plugin");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(commonConfig, {
+    mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        splitChunks: { chunks: "all" }
+    },
+});
