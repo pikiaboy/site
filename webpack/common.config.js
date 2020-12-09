ae33ceefae1b309aa34150aefaaddb672c103fd3
Thomas Pham <thomas.pham96@yahoo.com>
2020-12-08 23:05:29 -0800

    Adding Seamntic UI and neccesary webpack configs

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const path = require('path');
 
 module.exports = {
@@ -12,19 +13,32 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                test: /\.((c|sa|sc)ss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                include: path.resolve('node_modules')
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000
+                    }
+                }
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../src/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css'
         })
     ]
 };
